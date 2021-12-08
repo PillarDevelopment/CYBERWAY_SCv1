ea414dcd4a31b9e66f6d72241c8ff57b0521cde9
Pillardev <pillardev@protonmail.com>
2021-12-08 13:30:52 +0300

    farming

--- contracts/Farming.sol
+++ contracts/Farming.sol
@@ -7,47 +7,39 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Farming is Ownable{
 
-    struct UserInfo {
-        uint256 startBlock;
-        //bool isActive;
-    }
+    mapping(address => mapping(uint256 => uint256))public userInfo; //  user => (tokenId => timeToLock)
 
     IERC20 public cyberToken;
     ICyberWayNFT public cyberNft;
 
-    uint256 public basicLockPeriod;
-
-    uint256 private _tokenPerBlock = 2e18;
-
-    mapping (address => mapping (uint256 => UserInfo)) public userInfo;
+    uint256 public basicLockPeriod = 43200; //blocks -  14 sec per block / 7 days
+    uint256 private _tokenPerBlock = 2e18; // 2 tokens
 
     event NFTDeposited(address sender, uint256 id, uint256 startBlock);
     event NFTWithdrawn(address sender, uint256 id, uint256 amount);
     event NFTEmergencyWithdrawn(address sender, uint256 id, uint256 block);
     event NFTDeposited();
 
-    constructor(address nft, address token, uint256 newTokenPerBlock, uint256 newBasicLockPeriod) {
+    constructor(address nft, address token) {
         cyberNft = ICyberWayNFT(nft);
         cyberToken = IERC20(token);
-        _tokenPerBlock = newTokenPerBlock;
-        basicLockPeriod = newBasicLockPeriod;
     }
 
 
     function depositFarmingToken(uint256 id) public {
-        require(userInfo[msg.sender][id].startBlock != 0, "Farming: is exist");
+        require(userInfo[msg.sender][id] == 0, "Farming: is exist");
         cyberNft.safeTransferFrom(msg.sender, address(this), id);
-        userInfo[msg.sender][id].startBlock = block.number;
-        //
-        //
-        emit NFTDeposited(msg.sender, id, userInfo[msg.sender][id].startBlock);
+        userInfo[msg.sender][id] = block.number;
+        emit NFTDeposited(msg.sender, id, userInfo[msg.sender][id]);
     }
 
 
     function withdrawFarmingToken(uint256 id) public {
-        require(userInfo[msg.sender][id].startBlock + basicLockPeriod >= block.number, "Farming: incorrect period");
+        require(userInfo[msg.sender][id] + basicLockPeriod >= block.number, "Farming: incorrect period");
+        require(userInfo[msg.sender][id] != 0, "Farming: Sender isn't token's owner");
+
         uint256 amount = pendingToken(msg.sender, id);
-        userInfo[msg.sender][id].startBlock = 0;
+        userInfo[msg.sender][id] = 0;
 
         cyberToken.transferFrom(address(this), msg.sender, amount);
         cyberNft.safeTransferFrom(address(this), msg.sender, id);
@@ -66,15 +58,15 @@ contract Farming is Ownable{
 
 
     function emergencyWithdrawFarmingToken(uint256 id) public {
-        require(userInfo[msg.sender][id].startBlock != 0, "Farming: Sender isn't token's owner");
-        userInfo[msg.sender][id].startBlock = 0;
+        require(userInfo[msg.sender][id] != 0, "Farming: Sender isn't token's owner");
+        userInfo[msg.sender][id] = 0;
         cyberNft.safeTransferFrom(address(this), msg.sender, id);
         emit NFTEmergencyWithdrawn(msg.sender, id, block.number);
     }
 
 
-    function pendingToken(address _user, uint256 _id) public view returns (uint256) {
-        return(block.number - userInfo[_user][_id].startBlock) * basicLockPeriod;
+    function pendingToken(address user, uint256 id) public view returns (uint256) {
+        return (block.number - userInfo[user][id]) * basicLockPeriod;
     }
 
 
---
+++ contracts/mock/TokenMock.sol
@@ -0,0 +1,27 @@
+// "SPDX-License-Identifier: MIT"
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DcToken is ERC20, Ownable {
+    using Address for address;
+
+    constructor(string memory _name,
+        string memory _symbol)ERC20(_name, _symbol) {
+    }
+
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool) {
+        _mint(_to, _amount);
+        return true;
+    }
+
+
+    function burn(uint256 _amount) external onlyOwner returns(bool) {
+        _burn(msg.sender, _amount);
+        return true;
+    }
+
+}
